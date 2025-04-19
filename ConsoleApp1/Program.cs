a092901561a0a86050764fe7bc2ea12c03709eac
Joseph DeCommer <jdecommer@gmail.com>
2025-04-19 13:40:11 -0400

    Learning c#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// Console.Write("Enter your name: ");
+// var name = Console.ReadLine();
+// Console.WriteLine($"Hello, {name}!");
+
+var num1 = 5;
+int num2 = 10;
+
+int ans = num1 + num2;
+
+Console.WriteLine(ans);
+
